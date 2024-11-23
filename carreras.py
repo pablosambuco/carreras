54869735f6f42e6d492a3413e3753aa8825e4c19
Pablo <pablosambuco@gmail.com>
2024-11-22 23:53:57 -0300

    Update carreras.py

--- carreras.py
+++ carreras.py
@@ -62,7 +62,7 @@ class Game:
         self.min_row = 0
         self.top_card = None
 
-    def draw_game(self):
+    def draw_game(self): #TODO! Move to Board. Game should be agnostic of the final representation
         self.board.clear()
         box = self.board.draw_box(
             (self.length + 2) * Board.CARD_HEIGHT + 2,
